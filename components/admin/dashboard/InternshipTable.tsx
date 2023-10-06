c906d954090d7504e9ec44c9dbcfdcb1d917dcf6
Alan Varghese Paul <alanvarghesepaul29@gmail.com>
2023-10-06 23:09:54 +0530

    change4

--- components/admin/dashboard/InternshipTable.tsx
+++ components/admin/dashboard/InternshipTable.tsx
@@ -41,7 +41,7 @@ export interface InternUserTypes {
 }
 
 export async function getInternData(): Promise<InternUserTypes[]> {
-  const resp = await axios.get("http://localhost:3000/api/internUser");
+  const resp = await axios.get("/api/internUser");
   if (!resp.status) {
     throw new Error("Failed to fetch data");
   }
