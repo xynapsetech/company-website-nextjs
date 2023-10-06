179f81a52de42aec07ce244ae2f8dd92123e5735
Alan Varghese Paul <alanvarghesepaul29@gmail.com>
2023-10-06 21:04:27 +0530

    footer test change

--- components/admin/footer/AdminFooter.tsx
+++ components/admin/footer/AdminFooter.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function AdminFooter() {
   return (
     <div className='w-full h-16 bg-gray-200/50 dark:bg-slate-900/50 flex justify-center items-center text-gray-400 dark:text-gray-700'>
-      <p>XynapseTech Admin</p>
+      <p>XynapseTech Admin Panel</p>
     </div>
   )
 }
